373327edc229db7c2a59560763aeb85f498269ef
mam10023 <mam10023@nyu.edu>
2024-08-07 09:32:07 -0400

    Constructor created

---
+++ Email.java
@@ -0,0 +1,12 @@
+public class Email {
+    private String subject;
+    private String recipient;
+    private String emailContent;
+
+    public Email(String subject, String recipient, String emailContent) {
+        this.subject = subject;
+        this.recipient = recipient;
+        this.emailContent = emailContent;
+    }
+
+}
